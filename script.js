450d85874fcb65606b7e326d4039d14e8eeeeeb8
Matthew Jackson <mjackson2094@gmail.com>
2021-02-11 12:38:43 -0500

    Add comments

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const number = Math.floor(Math.random() * 10) + 1;
+// Assigns an array of values between 1 and 10
+const number = [ ...Array(10).keys() ].map(i => i+1);
+
 const color = ['blue','red','green','yellow','orange','purple','pink'];
 const animal = ['bat', 'bear','beetle','bull','deer','elephant','frog','goldfish','horse','lizard','pig','rabbit'];
 
+// Returns random value from entered array
 const getRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
 
 const messageGenerator = () => {
   console.log(
