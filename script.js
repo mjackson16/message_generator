3ce6f6311c02bc66f96b4baad4161623ff737c26
Matthew Jackson <mjackson2094@gmail.com>
2021-02-11 12:27:46 -0500

    Add getRandom and messageGenerator functions

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const number = Math.floor(Math.random() * 10) + 1;
 const color = ['blue','red','green','yellow','orange','purple','pink'];
 const animal = ['bat', 'bear','beetle','bull','deer','elephant','frog','goldfish','horse','lizard','pig','rabbit'];
 
+const getRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const messageGenerator = () => {
+  console.log(
+    `Greetings!\nYour lucky number is ${number}.\nYour lucky color is ${getRandom(
+      color
+    )}.\nYour lucky animal is a ${getRandom(animal)}.` +
+      `\nHope you have some good luck!`
+  );
+};
+
+messageGenerator();
