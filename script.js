80af725f32f891c91036359a5b358e2842e20568
Matthew Jackson <mjackson2094@gmail.com>
2021-02-11 12:25:08 -0500

    Add initial variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const number = Math.floor(Math.random() * 10) + 1;
+const color = ['blue','red','green','yellow','orange','purple','pink'];
+const animal = ['bat', 'bear','beetle','bull','deer','elephant','frog','goldfish','horse','lizard','pig','rabbit'];
+
